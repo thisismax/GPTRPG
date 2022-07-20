f1ce36571ceadef58fc96162988f03fc0361cad2
Max Reichlin <max.reichlin@gmail.com>
2022-07-19 21:54:23 -0700

    Found data solution for monsters, started script

--- SRDread/SRDread.py
+++ SRDread/SRDread.py
@@ -1,2 +1,22 @@
-# Use selenium perhaps?
-# This could also be a good cypress task
+# Script to reformat SRD Monster data for inclusion in fitted model
+# Input data: CSV, 1 row per monster
+# Output data: jsonl
+# Data source:
+    # Data scraped from aidedd.org by Travis Tyler
+    # (https://www.kaggle.com/datasets/travistyler/dnd-5e-monster-manual-stats)
+    # Stats collected here ("SRDmonsters.csv") are all from the D&D SRD and covered by
+    # the OGL (https://media.wizards.com/2016/downloads/SRD-OGL_V1.1.pdf)
+
+import pandas as pd
+from pathlib import Path
+
+FILE = Path('.')/'GPTRPG'/'SRDread'/'SRDmonsters.csv'
+
+df = pd.read_csv(FILE)
+
+print(df.columns)
+
+# tasks
+# determine proper final format
+# likely need to split up some columns:
+# senses, attributes, actions, legendary_actions
